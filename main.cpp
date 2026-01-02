44539985d284041316df770e6dfca3202e5ee691
Srikhar Shashi D <dasojusrikhargmail.com>
2026-01-03 01:32:30 +0530

    added time calc

--- main.cpp
+++ main.cpp
@@ -9,15 +9,18 @@
 
 int main() {
     constexpr size_t QUEUE_CAPACITY = 1000000;
-    constexpr size_t NUM_PARSERS = 8;
+    constexpr size_t NUM_PARSERS = 1;
 
-    //Step 1 --> Create Safe Queue
+    // Step 1 --> Create Safe Queue
     SafeQueue<std::string> queue(QUEUE_CAPACITY);
 
-    //Step 2 --> Create Thread Pool
+    // Step 2 --> Create Thread Pool
     ThreadPool pool(NUM_PARSERS + 1);
 
-    //Step 3 --> Start Parser Threads
+    // ⏱ Start timing
+    auto startTime = std::chrono::steady_clock::now();
+
+    // Step 3 --> Start Parser Threads
     std::vector<std::unique_ptr<LogParserThread>> parsers;
     parsers.reserve(NUM_PARSERS);
 
@@ -28,29 +31,41 @@ int main() {
         pool.submit(std::ref(*parsers.back()));
     }
 
-    //Step 4 --> Start Single Reade Thread
-    pool.submit(LogReaderThread("/Users/shashi/CLionProjects/multithreadedparser/logfiles.log", queue));
+    // Step 4 --> Start Single Reader Thread
+    pool.submit(LogReaderThread(
+        "/Users/shashi/CLionProjects/multithreadedparser/logfiles_1000000.log",
+        queue
+    ));
 
-    std::cout << "Before shutdown\n";
-    //Step 5  --> Wait for readers to finish --> Join all threads to main thread and wait for exit
+    // Step 5 --> Wait for all threads to finish
     pool.shutdown();
-    std::cout << "After shutdown\n";
 
+    // ⏱ End timing
+    auto endTime = std::chrono::steady_clock::now();
+    auto durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(
+        endTime - startTime
+    ).count();
 
-    //Step 6 --> Aggregate all the LogRecords from all shared pointers
+    // Step 6 --> Aggregate all the LogRecords
     LogAggregator aggregator;
+    size_t totalRecords = 0;
+
     for (const auto& parser : parsers) {
+        totalRecords += parser->records().size();
         aggregator.addBatch(parser->records());
     }
 
-    //Step 7 --> Write Parsed Logs to a file
+    // Step 7 --> Write Parsed Logs to a file
     writeParsedLogsToFile(
         "parsed_logs.csv",
         aggregator.records()
     );
 
-    std::cout << "Parsed records written to parsed_logs.csv\n";
+    // ⏱ Final output
+    std::cout << "Parsed " << totalRecords << " records in "
+              << durationMs << " ms \n" << "Used "<< NUM_PARSERS << " threads " " \n";
 
+    std::cout << "Parsed records written to parsed_logs.csv\n";
 
     return 0;
 }
